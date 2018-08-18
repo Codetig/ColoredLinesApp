db7b2bf9edba3e950defbbd2f982fbf37ca9737e
Ife Omoniyi <codetig@outlook.com>
2018-08-18 14:10:41 -0500

    Added capability to accept file path and display in multicolored lines.

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Displays the content of a given file in colored lines.
 ##Purpose
 It is sometimes hard to read text in for people with certain eyes defects,
 like astigmatism. This app shows the text with lines alternating colors.
-This should make it easier to follow a line and not jump from lines , which
+This should make it easier to follow a line and not jump from lines, which
 makes reading harder and more frustrating.
--- main.py
+++ main.py
@@ -1,5 +1,25 @@
 from termcolor import colored
 
 #Get file path from user
+given_path = input("File path of the file you want to read: ")
+
 #Make Filehandler
+try:
+  fhandler = open(given_path)
+except:
+  print('The filepath given was not found. Filepath =', given_path)
+  quit()
+
 #Display text from file in alternating colors
+color = 'green'
+for line in fhandler:
+  if(line.strip == ""):
+    print(line.rstrip())
+    continue
+  
+  print(colored(line.rstrip(), color))
+
+  if(color == 'green'):
+    color = 'yellow'
+  else:
+    color = 'green'
